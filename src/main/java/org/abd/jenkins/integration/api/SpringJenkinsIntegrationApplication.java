152f86b4249fdd58650760a0651cf4bce8675d9c
abdellah71 <b_abdellahdz@hotmail.com>
2022-09-22 15:44:34 -0400

    first commit

---
+++ src/main/java/org/abd/jenkins/integration/api/SpringJenkinsIntegrationApplication.java
@@ -0,0 +1,29 @@
+package org.abd.jenkins.integration.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsIntegrationApplication {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsIntegrationApplication.class);
+
+	@PostConstruct
+	public void initial(){
+
+		logger.info("Application started...");
+
+	}
+
+	public static void main(String[] args) {
+
+		logger.info("Application executed...");
+
+		SpringApplication.run(SpringJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/abd/jenkins/integration/api/SpringJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,21 @@
+package org.abd.jenkins.integration.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsIntegrationApplicationTests {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsIntegrationApplicationTests.class);
+
+
+	@Test
+	void contextLoads() {
+
+		logger.info("test case executing....");
+
+	}
+
+}
